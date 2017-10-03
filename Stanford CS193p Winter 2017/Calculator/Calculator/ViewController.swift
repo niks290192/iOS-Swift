bb3147f6fdf5d907289fd74e13e4b950c3039965
nikhilarora <nikhil290192@gmail.com>
2017-10-03 19:41:00 +0530

    Completed First Lecture
    Introduction to Xcode 8, iOS10 and Swift 3.

--- Stanford CS193p Winter 2017/Calculator/Calculator/ViewController.swift
+++ Stanford CS193p Winter 2017/Calculator/Calculator/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var display: UILabel!
+
+	var userIsInTheMiddleOfTyping:Bool = false
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +23,42 @@ class ViewController: UIViewController {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+
+
+	@IBAction func touchDigit(_ sender: UIButton) {
+		let digit = sender.currentTitle!
+		if userIsInTheMiddleOfTyping {
+			let textCurrentlyInDisplay = display.text!
+			display.text = textCurrentlyInDisplay + digit
+		} else {
+			display.text! = digit
+			userIsInTheMiddleOfTyping = true
+		}
+	}
+
+	var displayValue : Double {
+		get{
+			return Double(display.text!)!
+		}
+		set {
+			display.text = String(newValue)
+		}
+	}
+
+	@IBAction func preformOperation(_ sender: UIButton) {
+		userIsInTheMiddleOfTyping = false
+		if let mathematicalSymbol = sender.currentTitle {
+			switch mathematicalSymbol {
+			case "π":
+				displayValue = Double.pi
+			case "√":
+				displayValue = sqrt(displayValue)
+			default:
+				break
+			}
+		}
+	}
+
 
 
 }
