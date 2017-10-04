45d23fff52758cd736929808a607e9a29391f99d
nikhilarora <nikhil290192@gmail.com>
2017-10-04 12:02:10 +0530

    Second Chapter
    MVC, iOS, Xcode, Swift Demo

--- Stanford CS193p Winter 2017/Calculator/Calculator/ViewController.swift
+++ Stanford CS193p Winter 2017/Calculator/Calculator/ViewController.swift
@@ -45,17 +45,20 @@ class ViewController: UIViewController {
 		}
 	}
 
+	private var brain = CalculatorBrain()
+
 	@IBAction func preformOperation(_ sender: UIButton) {
-		userIsInTheMiddleOfTyping = false
+		if userIsInTheMiddleOfTyping {
+			brain.setOperand(displayValue)
+			userIsInTheMiddleOfTyping = false
+		}
+
 		if let mathematicalSymbol = sender.currentTitle {
-			switch mathematicalSymbol {
-			case "π":
-				displayValue = Double.pi
-			case "√":
-				displayValue = sqrt(displayValue)
-			default:
-				break
-			}
+			brain.preformOperation(mathematicalSymbol)
+		}
+
+		if let result = brain.result {
+			displayValue = result
 		}
 	}
 
