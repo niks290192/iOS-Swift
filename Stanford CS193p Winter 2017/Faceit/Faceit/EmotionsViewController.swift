59aeee77e75f66c4978bf055c52c71506b647037
nikhilarora <nikhil290192@gmail.com>
2017-12-11 21:32:23 +0530

    Completed Segue
    Completed Segue With PopoverViewController

--- Stanford CS193p Winter 2017/Faceit/Faceit/EmotionsViewController.swift
+++ Stanford CS193p Winter 2017/Faceit/Faceit/EmotionsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EmotionsViewController: VCLLoggingViewController {
+class EmotionsViewController: UITableViewController, UIPopoverPresentationControllerDelegate {
 
     // MARK: - Navigation
 
@@ -19,19 +19,47 @@ class EmotionsViewController: VCLLoggingViewController {
 			destinationViewController = navigationController.visibleViewController ?? destinationViewController
 		}
 		if let faceViewController = destinationViewController as? FaceViewController,
-			let identifier = segue.identifier,
-			let expression = emotinalFaces[identifier] {
-			faceViewController.expression = expression
+			let cell = sender as? UITableViewCell,
+			let indexPath = tableView.indexPath(for: cell){
+			faceViewController.expression = emotionFaces[indexPath.row].expression
 			faceViewController.navigationItem.title = (sender as? UIButton)?.currentTitle
-
+		} else if destinationViewController is ExpressionEditorViewControllerTableViewController {
+			if let popoverPresentationViewController = segue.destination.popoverPresentationController {
+				popoverPresentationViewController.delegate = self
+			}
 		}
 	}
 
-	private let emotinalFaces: Dictionary<String, FacialExpression> = [
-		"sad": FacialExpression(eyes: .closed, mouth: .frown),
-		"happy": FacialExpression(eyes: .open, mouth: .smile),
-		"worried": FacialExpression(eyes: .open, mouth: .smirk)
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return emotionFaces.count
+	}
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Emotion Cell", for: indexPath)
+		cell.textLabel?.text = emotionFaces[indexPath.row].name
+		return cell
+	}
+
+	@IBAction func addEmotinalFace(from segue: UIStoryboardSegue) {
+		if let editor = segue.source as? ExpressionEditorViewControllerTableViewController {
+			emotionFaces.append((editor.name, editor.facialExpression))
+			tableView.reloadData()
+		}
+	}
+
+	private var emotionFaces: [(name: String, expression:FacialExpression)] = [
+		("Sad", FacialExpression(eyes: .closed, mouth: .frown)),
+		("Happy", FacialExpression(eyes: .open, mouth: .smile)),
+		("Happy", FacialExpression(eyes: .open, mouth: .smirk))
 	]
 
-
+	func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+		if traitCollection.verticalSizeClass == .compact {
+			return .none
+		} else if traitCollection.horizontalSizeClass == .compact {
+			return .overFullScreen
+		} else {
+			return .none
+		}
+	}
 }
