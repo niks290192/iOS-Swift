bc5874146ef8b48e2079b483e9b512c483b0ca28
nikhilarora <nikhil290192@gmail.com>
2017-12-13 15:11:36 +0530

    Hello World
    Completed Basic UIView Animations.

--- iOS Animations/Hello World/Hello World/ViewController.swift
+++ iOS Animations/Hello World/Hello World/ViewController.swift
@@ -10,15 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	@IBOutlet weak var helloWorld: UILabel!
+	@IBOutlet weak var secondLabel: UILabel!
+	@IBOutlet weak var hiddenLabel: UILabel!
+
+
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		helloWorld.center.y -= view.bounds.height
+		secondLabel.center.y += view.bounds.height
+		hiddenLabel.alpha = 0.0
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+
+		UIView.animate(withDuration: 1.0, animations: {
+			self.helloWorld.center.y += self.view.bounds.height
+		}, completion: { finished in
+			 self.secondAnimation()
+		})
+
+		UIView.animate(withDuration: 2.0, delay: 0.0, options: [], animations: {
+			self.view.backgroundColor = UIColor.yellow
+		}, completion: nil)
 	}
+
+	private func secondAnimation() {
+		UIView.animate(withDuration: 1.0, delay: 0.5, options: [], animations: {
+			self.secondLabel.center.y -= self.view.bounds.height
+		}, completion: { finished in
+			self.backgroundColor()
+		})
+	}
+
+	private func backgroundColor() {
+		UIView.animate(withDuration: 2.5, animations: {
+			self.view.backgroundColor = UIColor.black
+		}, completion: nil)
+
+
+		UIView.animate(withDuration: 1.0, delay: 1.5, options: [], animations: {
+			self.hiddenLabel.alpha = 1.0
+		}, completion: nil)
+
+	}
+
 
 
 }
