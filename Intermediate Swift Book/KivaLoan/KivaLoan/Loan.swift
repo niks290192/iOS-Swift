bf7aaf430b6b26254ee633f36d5e4f3226711702
NiKhil AroRa <nikhil290192@gmail.com>
2018-12-14 22:38:49 +0530

    Chapter 4 completed

---
+++ Intermediate Swift Book/KivaLoan/KivaLoan/Loan.swift
@@ -0,0 +1,18 @@
+//
+//  Loan.swift
+//  KivaLoan
+//
+//  Created by Simon Ng on 2/10/2015.
+//  Copyright © 2015 AppCoda. All rights reserved.
+//
+
+import Foundation
+
+class Loan {
+    
+    var name:String = ""
+    var country:String = ""
+    var use:String = ""
+    var amount:Int = 0
+    
+}
